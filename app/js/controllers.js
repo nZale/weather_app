de76b477dc3141597ed9d905539cdddd5d4a9e8a
Gabriel Andrei <gabriel.andrei.sv@gmail.com>
2015-02-14 18:44:43 +0100

    upload project
    initial upload

---
+++ app/js/controllers.js
@@ -0,0 +1,67 @@
+  	// search API params
+	var app = angular.module('twitterApp', []); // register module
+
+	function mainController($scope, $http, $interval) {
+	  $scope.query;
+	  $scope.refreshInterval = 10;
+	  $scope.weather_data;	
+	  $scope.weather_list = [];
+	  $scope.date;
+	  $scope.count = 0;
+	  $scope.greeting = 'Weather information';
+	  $scope.predicate = '-retweet_count';
+
+
+	  // connect to the server and setup listener
+	  function connect()
+	  {
+	  	if(io !== undefined) 
+	  	{
+	  		// server connection
+		    var socket = io.connect('http://localhost:8080');
+
+		    // set listener for 'twitter-stream' message that will store incoming data
+		    socket.on('sending-data', function (data) {
+		    	//console.log(getDateTime() + ' c : getting tweets');
+		    	var weather_info = JSON.parse(data);
+		    	if(weather_info.weather == "")
+		    		return;
+    			getTime();
+		    	$scope.weather_list.push(weather_info);
+		    	$scope.count = $scope.weather_list.length;
+		    	$scope.$apply();
+
+		    });
+
+		    // if the connection is made it will request for tweets by sending "get tweets" message
+		    socket.on("connected", function(r) {
+		   		//console.log(getDateTime() + ' c : connected!');
+		     	socket.emit("get-data");
+		    });
+
+		    // 
+		    socket.on('disconnect', function () {
+			    socket.emit('client disconnected');
+			    //console.log(getDateTime() + ' c : client disconnected!');
+			});
+		}
+	  }
+
+      //get current time
+      function getTime() {
+	    $scope.date = new Date().toLocaleTimeString();
+	  }
+
+	  function getDateTime()
+	  {
+	  	return (new Date()).toUTCString(); 
+	  }
+
+	  connect();
+      //getTime();
+
+      //refresh tweets
+      $interval(function() {
+	    //getTime();
+	  	}, $scope.refreshInterval * 1000 * 60);
+	}
---
+++ server.js
@@ -0,0 +1,131 @@
+    var express  = require('express');
+    var app      = express();                            
+    var bodyParser = require('body-parser');
+    var http = require('http');
+    var schedule = require('node-schedule');
+    var server = http.createServer(app);
+    var io = require('socket.io').listen(server);
+    var https = require('https');
+    var request = require('request');
+
+    // configuration 
+    app.use(express.static(__dirname + '/app'));                 // set the static files location /public/img will be /img for users
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+
+    // start server ======================================
+    server.listen(8080);
+    console.log("App listening on port 8080");
+
+    // web main page
+    app.get('*', function(req, res) {
+        res.sendFile(__dirname + '/app/index.html');
+    });
+
+    var rule = new schedule.RecurrenceRule();
+    rule.minute = new schedule.Range(0, 59, 5);
+
+    var job = schedule.scheduleJob(rule, function(){
+      getWeather();
+      console.log("Job done @ " + new Date().getMinutes());
+    });
+
+
+    var weatherModel = function(){
+      model = this;
+      model.Location = '';
+      model.Weather = '';
+      model.Temperature = '';
+      model.Humidity = '';
+      model.TemperatureDif = 0;
+      model.SuggestionCode = '';
+      model.ChangeCode = "c_code_0.png";
+      model.WeatherImage;
+    }
+
+    var weatherViewModel = function(model)
+    {
+        vm = this;
+        w = model;
+        vm.currentTemperature = 0;
+        vm.setTemperatureDifference = function(num1, num2){
+          return Math.ceil((num1 < num2)? num1-num2 : num2-num1 * 100)/100; ;
+        };
+        vm.ToString = function() {
+          return "T: " + w.Temperature + " C: " + vm.currentTemperature + " D:" + w.TemperatureDif + " CD:" + w.ChangeCode;
+        };
+        vm.Suggestion = function() {
+          if(model.Temperature <= 10)
+            return "s_code_0.jpg";
+          else if(model.Temperature > 10 && model.Temperature <= 20)
+            return "s_code_1.jpg";
+          else if(model.Temperature > 20 && model.Temperature <= 30)
+            return "s_code_2.jpg";
+          else if(model.Temperature > 30)
+            return "s_code_3.jpg";
+        };
+        vm.setData = function(value){
+          w.Location = value.display_location.full;
+          w.Weather = value.weather;
+          w.Temperature = value.temp_c;
+          w.Humidity = value.relative_humidity;
+          w.TemperatureDif = vm.setTemperatureDifference(vm.currentTemperature, w.Temperature);
+          vm.currentTemperature = w.Temperature;
+          w.SuggestionCode = vm.Suggestion();
+          w.ChangeCode = vm.setChangeCode();
+          w.WeatherImage = value.icon_url;
+        }
+        vm.toJSON = function() {
+          return JSON.stringify({
+            location : w.Location,
+            weather : w.Weather,
+            temperature : w.Temperature,
+            humidity : w.Humidity,
+            temp_dif : w.TemperatureDif,
+            suggestion_code : w.SuggestionCode,
+            change_code : w.ChangeCode,
+            weather_image : w.WeatherImage
+           }, null, '');
+        };
+        vm.setChangeCode = function(){
+          if(w.TemperatureDif == 0)
+            return "c_code_0.png";
+          else if(w.TemperatureDif < 0)
+            return "c_code_1.png";
+          else if(w.TemperatureDif > 0)
+            return "c_code_2.png";
+        }
+    }
+
+    var weather = new weatherModel();
+    var weatherVM = new weatherViewModel(weather);
+
+
+    var getWeather = function(){ request('http://api.wunderground.com/api/43cce2bf1b0df44a/conditions/q/Norway/Oslo.json', function (error, response, data) {
+        if (!error && response.statusCode == 200) {
+          var result = JSON.parse(data);
+          weatherVM.setData(result.current_observation);
+          console.log(weatherVM.ToString());
+          console.log(weatherVM.Suggestion())
+          io.emit('sending-data', weatherVM.toJSON());
+        }
+      })
+    }
+
+    // create connection
+    io.sockets.on('connection', function (socket) 
+    {
+      socket.on("get-data", function() 
+      {
+        //console.log((new Date()).toUTCString() + ' s : send tweets');    
+        socket.broadcast.emit("sending-data", weatherVM.toJSON());              
+        socket.emit('sending-data', weatherVM.toJSON());
+        console.log((new Date()).toUTCString() + ' s : data sent!');       
+      })
+
+      // signal that client is connected
+      //console.log((new Date()).toUTCString() + ' s : connected!');
+      socket.emit("connected");
+    });
+
+  
